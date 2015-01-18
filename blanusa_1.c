e0d0d3544089da9379898c014f956e4432d1baba
Dave Taht <dave.taht@gmail.com>
2015-01-17 22:58:56 -0800

    Correct blanusa code - add wolfram alpha version

--- blanusa_1.c
+++ blanusa_1.c
@@ -22,34 +22,45 @@
 
 */
 
+
 /* compiled without -ffast-math, this inserts tons of calls to pow.
    with fast-math, it looks like it does the right thing with CSE */
 
 double blanusa_pow(double x) {
-	double r = (x-3)*pow(x-1,3) *
-		   (x+1)*(x+2)*(pow(x,4)+pow(x,3)-pow(7*x,2)-5*x+6)*
-		   pow((pow(x,4)+pow(x,3)-pow(5*x,2)-3*x+4),2) *
-    		   pow((x-3)*(x-1),3)*(pow(x,3)+pow(2*x,2)-3*x-5)*
-		   (pow(x,3)+pow(2*x,2)-x-1)*(pow(x,4) +
-		   pow(x,3)-pow(7*x,2)-6*x+7)*(pow(x,4) +
-		   pow(x,3)-pow(5*x,2)-4*x+3);
-	return r;
+  double r = (x-3)*pow(x-1,3) *
+    (x+1)*(x+2)*
+    (pow(x,4)+pow(x,3)-7*pow(x,2)-5*x+6)*
+    pow((pow(x,4)+pow(x,3) - 5 * pow(x,2)-3*x+4),2) *
+    (x-3)*pow(x-1,3)*(pow(x,3)+2*pow(x,2)-3*x-5) *
+    (pow(x,3)+2*pow(x,2)-x-1) *
+    (pow(x,4) + pow(x,3)- 7* pow(x,2)-6*x+7)*
+    (pow(x,4) + pow(x,3)- 5* pow(x,2)-4*x+3);
+  return r;
 }
 
-/* try expand not using pow
-double blanusa_unrolled(double x) {
-		   (x-3)*(x-1)*(x-3)*(x-1)*(x-3)*(x-1)
-		   (x+1)(x+2)(x^4+x^3-7x^2-5x+6)(x^4+x^3-5x^2-3x+4)^2
-    		   (x-3)(x-1)^3(x^3+2x^2-3x-5)
-		   (x^3+2x^2-x-1)(x^4+x^3-7x^2-6x+7)(x^4+x^3-5x^2-4x+3);
-}
+/* Wolfram alpha translated this into something different - as did my emacs,
+   when entered into a comment:
+
+(x-3)²(x-1)⁶(x+1)(x+2)(x³+2x²-3x-5)(x³+2x²-x-1)(x⁴+x³-7x²-6x+7)
+(x⁴+x³-7x²-5x+6)(x⁴+x³-5x²-4x+3)(x⁴+x³+5x²-3x+4)²
+
 */
 
-
+double blanusa_wolfram(double x) {
+  double r = pow(x-3,2)*pow(x-1,6)*(x+1)*(x+2)*
+    (pow(x,3)+2*pow(x,2)-3*x-5)*
+    (pow(x,3)+2*pow(x,2)-x-1)*
+    (pow(x,4)+pow(x,3)-5*pow(x,2)+6*x+7)*
+    (pow(x,4)+pow(x,3)-7*pow(x,2)-5*x+6)*
+    (pow(x,4)+pow(x,3)-5*pow(x,2)+3) *
+    pow(pow(x,4)+pow(x,3)-5*pow(x,2)+3*x+4,2);
+  return r;
+}
 
 void timespec_diff(struct timespec *temp, struct timespec *start, struct timespec *end)
 {
-	if ((end->tv_nsec-start->tv_nsec)<0) {
+  //  if(temp && start && end) return;
+  if ((end->tv_nsec-start->tv_nsec)<0) {
 		temp->tv_sec = end->tv_sec-start->tv_sec-1;
 		temp->tv_nsec = 1000000000 + end->tv_nsec-start->tv_nsec;
 	} else {
@@ -58,21 +69,48 @@ void timespec_diff(struct timespec *temp, struct timespec *start, struct timespe
 	}
 }
 
+/* Declaring these, this large, in main, causes a segfault */
+
+double results[MAXB];
+double results2[MAXB];
+
 /* A sufficiently smart compiler would replace all this with a single
    call to printf, doing all the calculations at compile time. That
    doesn't seem to happen... */
 
+
 int main(int argc, char **argv) {
-	double results[MAXB];
 	struct timespec end;
 	struct timespec start;
 	struct timespec dur;
+
 	if(clock_gettime(DEF_TIMER,&start) == -1) {
 		perror("Bad clock");
 		exit(-1);
 	}
-	for (int i = 0; i < MAXB; i++) {
+
+	for (int i = 8; i < MAXB; i++) {
 		results[i] = blanusa_pow(i);
+	}
+
+	if(clock_gettime(DEF_TIMER,&end) == -1) {
+		perror("Bad clock");
+		exit(-1);
+	}
+
+	timespec_diff(&dur,&start,&end);
+
+	printf("Duration: %ld.%09ld\n",
+			   dur.tv_sec, dur.tv_nsec);
+
+	/* Try the alternate version */
+	
+	if(clock_gettime(DEF_TIMER,&start) == -1) {
+		perror("Bad clock");
+		exit(-1);
+	}
+	for (int i = 8; i < MAXB; i++) {
+		results2[i] = blanusa_wolfram(i);
 	}
 
 	if(clock_gettime(DEF_TIMER,&end) == -1) {
@@ -83,8 +121,9 @@ int main(int argc, char **argv) {
 	timespec_diff(&dur,&start,&end);
 	printf("Duration: %ld.%09ld\n",
 			   dur.tv_sec, dur.tv_nsec);
-	for (int i = 0; i < MAXB; i++) {
-		printf("%g\n", results[i]);
+	for (int i = 8; i < MAXB; i++) {
+	  printf("%s %g %g\n", results[i] == results2[i] ? "GOOD" : "BAD ",
+		 results[i], results2[i]);
 	}
 }
 
